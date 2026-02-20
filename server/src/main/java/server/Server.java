1674c4ddbb637f3d0cc9d4c3122d9b4b237ba19a
JASON-B0URNE <stb56@byu.edu>
2026-02-19 19:47:27 -0700

    Formed Intial Endpoints

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -7,10 +7,39 @@ public class Server {
     private final Javalin javalin;
 
     public Server() {
-        javalin = Javalin.create(config -> config.staticFiles.add("web"));
+        javalin = Javalin.create(config -> {config.staticFiles.add("web");})
+            .delete("/db", ctx -> {
 
-        // Register your endpoints and exception handlers here.
+            })
+            .post("/user", ctx -> {
 
+            })
+            .post("/session", ctx -> {
+
+            })
+            .delete("/session", ctx -> {
+
+            })
+            .get("/game", ctx -> {
+                String authToken = ctx.header("authorization");
+
+                if (authToken == null) {
+                    ctx.status(401).result("Error: unauthorized");
+                }
+
+            })
+            .post("/game", ctx -> {
+
+            })
+            .put("/game", ctx -> {
+
+            })
+
+
+
+
+
+        ;
     }
 
     public int run(int desiredPort) {
