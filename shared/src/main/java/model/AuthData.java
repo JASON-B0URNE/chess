bed4cb44eac9c1856c8fd04b8aa66c0cc879df1b
JASON-B0URNE <stb56@byu.edu>
2026-02-19 19:53:42 -0700

    Finished declaring the public records class

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,11 +1,4 @@
 package model;
 
-public class AuthData {
-    public String authToken;
-    public String username;
+public record AuthData(String authToken, String username) {}
 
-    public AuthData(String authToken, String username) {
-        this.authToken = authToken;
-        this.username = username;
-    }
-}
